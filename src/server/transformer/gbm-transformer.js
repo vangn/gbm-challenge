112a6edfbf68badc349a776a28baeaff029fab39
Iván García <cvan.gn@gmail.com>
2018-10-25 00:32:47 -0500

    GBM/ Fix error API null response

--- src/server/transformer/gbm-transformer.js
+++ src/server/transformer/gbm-transformer.js
@@ -1,7 +1,7 @@
 class GBMTransformer {
 
     transformGBMResulData(resultObj) {
-        if (resultObj === undefined) {
+        if (resultObj === undefined || resultObj === null) {
             return [];
         }
 
